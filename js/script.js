ce4008417174cf94958adc0868ed4bb4f24d9f23
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-04-30 02:07:32 +0000

    add first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateCost() {
+  const sizeOfPizza = document.getElementById("medium-size-pizza").checked
+  const numberOfTopping = parseInt(
+    document.getElementById("number-of topping").value
+  )
+
+  if (sizeOfPizza == true) {
+    const totalCost = 5 + 0.75 * numberOfTopping
+    const totalCostWithTaxes = (totalCost * 13) / 100 + totalCost
+    document.getElementById("result").innerHTML =
+      "The total cost is : " + totalCostWithTaxes.toFixed(2)
+  } else {
+    const totalCost = 10 + 0.75 * numberOfTopping
+    const totalCostWithTaxes = (totalCost * 13) / 100 + totalCost
+    document.getElementById("result").innerHTML =
+      "The total cost with taxes is  is : " + totalCostWithTaxes.toFixed(2)
+  }
+}
